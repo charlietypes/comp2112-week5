96c1670f6db6ee57635683894cebf67c35468a71
charlietypes <@Belinda.Travers@MyGeorgian.ca>
2022-10-01 13:24:40 -0400

    updated project files

--- scripts/app.js
+++ scripts/app.js
@@ -2,58 +2,15 @@
 //not allowed to use var in code in JS course
 //IIFE -- Immediately Invoked Function Expression
 //AKA - Self Executing Function
+//do not code in app.js file, just app.ts file
 (function () {
-    /**
-     * This function loads data asynchronously from a URL.
-     * It calls the callback function when the data loading is complete
-     *
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
-     */
-    function LoadData(method, url, callback) {
-        //Step 1- Create an empty XHR object
-        let XHR = new XMLHttpRequest();
-        //Step 2- Compose the Request
-        XHR.open(method, url);
-        //Step 3- Send the request to the server
-        XHR.send();
-        //Step 4- Set up an event listener
-        XHR.addEventListener("readystatechange", function () {
-            if (XHR.status == 200 && XHR.readyState == 4) {
-                //console.log(XHR.responseText); //200= everything is ok, ready state of 4= everything is loaded
-                // let contactDataSource = JSON.parse(XHR.responseText);
-                // console.log(contactDataSource.ContactList[0]);
-                //return XHR.responseText;
-                callback(XHR.responseText);
-            }
-        });
-    }
+    //First way of using functions - named function
     function Start() {
         console.log("App Started!");
-        // LoadData("GET", "/Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
-        //this is jQuery
-        $.getJSON("/Data/contacts.json", function (DataSource) {
+        $.getJSON("./Data/contacts.json", function (DataSource) {
             console.log(DataSource.ContactList[0]);
         });
-    }
+    } //btw ctrl-/ to comment multiple things out at once
     window.addEventListener("load", Start);
-    //First way of using functions - named function
-    //  function Start()
-    //  {
-    //     console.log("App Started!");
-    //  } //btw ctrl-/ to comment multiple things out at once
-    // //Second way of using functions - anonymous function
-    // let Start = function()
-    // {
-    //     console.log("App Started!");
-    // }
-    //Third  way of using functions - arrow function
-    // let Start = () => 
-    // {
-    //     console.log("App Started!");
-    // }
 })();
 //# sourceMappingURL=app.js.map
--- scripts/app.ts
+++ scripts/app.ts
@@ -2,79 +2,21 @@
 //not allowed to use var in code in JS course
 //IIFE -- Immediately Invoked Function Expression
 //AKA - Self Executing Function
+//do not code in app.js file, just app.ts file
 
 (function()
 {
-    /**
-     * This function loads data asynchronously from a URL.
-     * It calls the callback function when the data loading is complete
-     * 
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
-     */
-
-    function LoadData(method: string, url: string, callback: Function):void //XHR - allows you to get data from a server without doing a full page refresh // we right clicked here and selected rename symbol and changed it from XHRRequest to LoadData
-    {
-        //Step 1- Create an empty XHR object
-        let XHR = new XMLHttpRequest();
-
-        //Step 2- Compose the Request
-        XHR.open(method, url);
-
-        //Step 3- Send the request to the server
-        XHR.send();
-
-        //Step 4- Set up an event listener
-        XHR.addEventListener("readystatechange", function(){
-
-        if(XHR.status == 200 && XHR.readyState == 4)
-        {
-            //console.log(XHR.responseText); //200= everything is ok, ready state of 4= everything is loaded
-            // let contactDataSource = JSON.parse(XHR.responseText);
-            // console.log(contactDataSource.ContactList[0]);
-            //return XHR.responseText;
-            callback(XHR.responseText);
-        }
-
-    });
-
-    }
-
-    function Start()
-      {
-        console.log("App Started!");
-        // LoadData("GET", "/Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
-
-        //this is jQuery
-        $.getJSON("/Data/contacts.json", function(DataSource){
-            console.log(DataSource.ContactList[0]);
-        });
-     }
-
-    window.addEventListener("load", Start);
 
     //First way of using functions - named function
 
-    //  function Start()
-    //  {
-    //     console.log("App Started!");
-    //  } //btw ctrl-/ to comment multiple things out at once
+      function Start()
+      {
+         console.log("App Started!");
 
-    // //Second way of using functions - anonymous function
-    // let Start = function()
-    // {
-    //     console.log("App Started!");
-    // }
+         $.getJSON("./Data/contacts.json", function(DataSource){
+            console.log(DataSource.ContactList[0]);
+         });
+      } //btw ctrl-/ to comment multiple things out at once
 
-    //Third  way of using functions - arrow function
-    // let Start = () => 
-    // {
-    //     console.log("App Started!");
-    // }
-
-    
-
+      window.addEventListener("load", Start);
 })();
